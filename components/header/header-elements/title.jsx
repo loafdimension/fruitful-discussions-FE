31a54ed65758b41daa01e2552480d69e7dcfedb4
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-07-22 18:39:12 +0200

    Creating a link in the header title so that from any page the user can select the title and be routed back to the homepage

--- components/header/header-elements/title.jsx
+++ components/header/header-elements/title.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Title() {
-  return <h1 className="header-title">FRUITFUL DISCUSSIONS</h1>;
+  return (
+    <Link to={`/`} className="header-title">
+      <h1>FRUITFUL DISCUSSIONS</h1>
+    </Link>
+  );
 }
 
 export default Title;
